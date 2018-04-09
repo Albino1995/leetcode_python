a113b4326c8b179e8a608685e36522c70c7cdfe9
Albino1995 <763968024@qq.com>
2018-04-09 17:28:17 +0800

    update 1

--- 1/Two Sum.py
+++ 1/Two Sum.py
@@ -2,9 +2,33 @@
 __author__ = 'Albino'
 
 class Solution:
+    # def twoSum(self, nums, target):
+    #     """
+    #     :type nums: List[int]
+    #     :type target: int
+    #     :rtype: List[int]
+    #     """
+    #     for i in nums:
+    #         a = nums.index(i)
+    #         try:
+    #             b = nums.index(target - i)
+    #         except:
+    #             continue
+    #         if a == b and nums.count(i) == 1:
+    #             continue
+    #         elif a == b:
+    #             nums.remove(i)
+    #             b = nums.index(target - i) + 1
+    #         return [a, b]
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        d = {}
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in d:
+                return [d[m], i]
+            else:
+                d[n] = i
+
+
+s = Solution()
+print(s.twoSum([2, 7, 11, 15], 9))
