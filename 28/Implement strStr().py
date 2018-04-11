a4b9fa564630f442caa2fbfe581d4fa28289a829
Albino1995 <763968024@qq.com>
2018-04-11 14:12:50 +0800

    update 28

--- 28/Implement strStr().py
+++ 28/Implement strStr().py
@@ -3,17 +3,18 @@ __author__ = 'Albino'
 
 
 class Solution:
+    # def strStr(self, haystack, needle):
+    #     """
+    #     :type haystack: str
+    #     :type needle: str
+    #     :rtype: int
+    #     """
+    #     if needle not in haystack:
+    #         return -1
+    #     return haystack.index(needle)
+
     def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        if needle not in haystack:
-            return -1
-        return haystack.index(needle)
-
-
+        return haystack.find(needle)
 
 s = Solution()
-print(s.strStr(haystack="qqqaaaaa", needle="aaaaaa"))
+print(s.strStr(haystack="qqqaaaaa", needle="aaaaa"))
